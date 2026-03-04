239a3aa24fb72b7f64ceda449d556e3587866d46
Shiba256 <153472750+Tomoyuki-Kawaguchi@users.noreply.github.com>
2026-03-04 20:52:17 +0900

    不要なimportを削除

--- backend/src/main/java/jp/tubeboard/features/auth/AuthController.java
+++ backend/src/main/java/jp/tubeboard/features/auth/AuthController.java
@@ -4,7 +4,6 @@ import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
