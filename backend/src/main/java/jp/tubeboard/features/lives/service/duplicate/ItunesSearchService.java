59b4d3946bc9a7938d17d3648fcae16aa5db0eaf
Shiba256 <153472750+Tomoyuki-Kawaguchi@users.noreply.github.com>
2026-03-31 09:51:31 +0900

    fix

--- backend/src/main/java/jp/tubeboard/features/lives/service/duplicate/ItunesSearchService.java
+++ backend/src/main/java/jp/tubeboard/features/lives/service/duplicate/ItunesSearchService.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
